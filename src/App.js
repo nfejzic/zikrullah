380e0b65d22c5330db3a39743fd3aeb39aec2452
Nadir Fejzic <nadirfejzo@gmail.com>
2019-07-11 17:04:21 +0200

    React State uses now array of objects with properties such as name, transcription, svg file etc. Changes to Stylesheet. Whole App acts as a button for morphing now.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
-// import { interpolate } from "flubber";
-// import { timeline } from "just-animate";
 
 //Morpher component
 import Morpher from "./components/Morpher";
 
-// Custom SVGs
-// import { ReactComponent as AllahCustom } from "./resources/SVG-custom/EH-Artboard 1.svg";
-// import { ReactComponent as ArRahmaanCustom } from "./resources/SVG-custom/EH-Artboard 2.svg";
-// Custom SVGs with holes as separate paths
-import "./style/main.scss";
-
-// function interpolatePoly(from, to) {
-//   return interpolate(from, to);
-// }
+import "./assets/style/main.scss";
 
 function App() {
   return (
--- src/components/Morpher.js
+++ src/components/Morpher.js
@@ -1,37 +1,52 @@
 import React from "react";
-// interpolate function from flubber. Used for interpolayion/morphing between two SVG pahts
-// import Flubber from "flubber";
-// import { interpolate } from "flubber";
+
 // timeline from just-animate. Used for animation. In this particular case, it's used for playing back SVG interpolation made by flubber
 import { timeline, interpolate } from "just-animate";
 
-import "../style/main.scss";
-// import { type } from "os";
-// import { type } from "os";
+import "../assets/style/main.scss";
 
+// import Flubber, library used for SVG Morphing animation / interpolation
 let flubber = require("flubber");
 
 class Morpher extends React.Component {
   constructor() {
     super();
-    this.state = { svgs: [] };
+    let svgJSON = require("../assets/data/esmaul-husna.json").svgs;
+
+    this.state = { svgData: [] };
+    for (let data of svgJSON) {
+      this.state.svgData.push(data);
+    }
+
+    // this.state = { svgs: [], svgData: [] };
     this.counter = 0;
+
+    // @TODO implement this.state as objects.
+    // Objects should have following values:
+    // 1. SVG - loaded SVG file
+    // 2. Name - Transcription of arabic name
+    // 3. Meaning - The meaning of the arabic name
   }
 
   componentDidMount() {
     this.loadSVG();
-
-    // this.setupSVGs(this.state.svgs);
   }
 
   loadSVG() {
     let svgPlaceholder = [];
     let svgPathsArray = [];
 
-    for (let i = 1; i <= 99; i++) {
-      let path = require("../resources/SVG_final/EsmaulHusna/" + i + ".svg");
+    // Load array of svg objects from JSON file
+    let svgData = require("../assets/data/esmaul-husna.json").svgs;
 
-      svgPathsArray[i - 1] = path;
+    //store paths into a paths array - this is used to load all the svgs
+    for (let i = 0; i < 99; i++) {
+      //insert fileName into path. File name is stored as fileNum property in JSON
+      let path = require("../assets/resources/SVG_final/EsmaulHusna/" +
+        svgData[i].fileNum +
+        ".svg");
+
+      svgPathsArray[i] = path;
     }
 
     this.getSVGs(svgPathsArray, svgPlaceholder);
@@ -44,11 +59,25 @@ class Morpher extends React.Component {
       client.onload = () => {
         pathsArray.shift();
         svgPlaceholder.push(this.createSVGfromString(client.responseText));
-        this.setState({ svgs: svgPlaceholder });
+
+        let index = svgPlaceholder.length - 1;
+
+        //copy the state
+        let stateCopy = { ...this.state.svgData };
+
+        //copy the item to modify
+        let dataItem = stateCopy[index];
+
+        //add SVG to dataItem
+        dataItem.svg = svgPlaceholder[index];
+
+        //add SVG item to copy of State, and then set the State to the new item
+        stateCopy[index] = dataItem;
+        this.setState(stateCopy);
+
+        // this.setState({ svgs: svgPlaceholder });
         if (pathsArray.length !== 0) {
           this.getSVGs(pathsArray, svgPlaceholder);
-        } else {
-          // this.setupSVGs(this.state.svgs);
         }
       };
       client.send();
@@ -67,23 +96,14 @@ class Morpher extends React.Component {
       svgArray[i].setAttribute("id", "EH-" + (i + 1));
     }
     this.setState(svgArray);
-    console.log("Success!");
   }
 
   morphSVG(fromSVGID, toSVGID) {
     let fromSVG = document.getElementById(fromSVGID);
     let toSVG = document.getElementById(toSVGID);
 
-    let paths = document.getElementsByTagName("path");
-    let fromPaths = [];
-    let toPaths = [];
-
-    for (let p of paths) {
-      let parent = p.parentNode.parentNode;
-      if (parent.nodeName === "g") parent = parent.parentNode;
-      if (parent.id === fromSVGID) fromPaths.push(p);
-      if (parent.id === toSVGID) toPaths.push(p);
-    }
+    let fromPaths = Array.from(fromSVG.getElementsByTagName("path"));
+    let toPaths = Array.from(toSVG.getElementsByTagName("path"));
 
     if (fromPaths.length - toPaths.length !== 0) {
       let case1 = fromPaths.length < toPaths.length;
@@ -137,52 +157,36 @@ class Morpher extends React.Component {
   morphSVGElementToArray(parentElement, toSVGindex) {
     let container = document.getElementById(parentElement);
     let fromSVG = container.firstChild;
+    let toSVG = this.state.svgData[toSVGindex].svg;
+
+    let fromPaths = Array.from(fromSVG.getElementsByTagName("path"));
+    let toPaths = Array.from(toSVG.getElementsByTagName("path"));
+
+    this.equalizeNumOfPaths(
+      { parentSVG: fromSVG, paths: fromPaths },
+      { parentSVG: toSVG, paths: toPaths }
+    );
+
+    //sort the paths here, from smallest to largest!
+    // fromPaths.sort((a, b) => this.sortString(a, b));
+    // toPaths.sort((a, b) => this.sortString(a, b));
+
+    this.animatePaths(
+      { parentSVG: fromSVG, paths: fromPaths },
+      { parentSVG: toSVG, paths: toPaths },
+      2000
+    );
+  }
+
+  animatePaths(from, to, duration) {
+    let fromPaths = from.paths;
+    let fromSVG = from.parentSVG;
+
+    let toPaths = to.paths;
+    let toSVG = to.parentSVG;
+
     let fromSVGID = fromSVG.id;
-    let toSVG = this.state.svgs[toSVGindex];
-    // let toSVGID = toSVG.id;
-
-    // let paths = document.getElementsByTagName("path");
-    let fromNodes = fromSVG.getElementsByTagName("path");
-    let toNodes = toSVG.getElementsByTagName("path");
-
-    let fromPaths = [];
-    for (let path of fromNodes) fromPaths.push(path);
-
-    let toPaths = [];
-    for (let path of toNodes) toPaths.push(path);
-
-    if (fromPaths.length - toPaths.length !== 0) {
-      let case1 = fromPaths.length < toPaths.length;
-      let case2 = fromPaths.length > toPaths.length;
-      let diff = case1
-        ? toPaths.length - fromPaths.length
-        : case2
-        ? fromPaths.length - toPaths.length
-        : 0;
-      for (let i = 0; i < diff; i++) {
-        let elemPath = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "path"
-        );
-        let copyPath = case1
-          ? fromPaths[fromPaths.length - 1]
-          : toPaths[toPaths.length - 1];
-        elemPath.setAttributeNS(null, "d", copyPath.getAttribute("d"));
-        if (case1) fromSVG.getElementsByTagName("g")[0].appendChild(elemPath);
-        if (case2) toSVG.getElementsByTagName("g")[0].appendChild(elemPath);
-        // fromSVG.appendChild(elemPath);
-        // (case1 ? fromPaths : toPaths).push(elemPath);
-        // console.log(fromPaths);
-        console.log(toPaths[0]);
-        if (case1) fromPaths.push(elemPath);
-        if (!case1) toPaths.push(elemPath);
-
-        // console.log(elemPath);
-      }
-    }
-
     let t1 = timeline();
-    let duration = 200; // in miliseconds
 
     for (let i = 0; i < fromPaths.length; i++) {
       let fromPathID = (fromPaths[i].id = fromSVGID + "Path" + i);
@@ -205,8 +209,9 @@ class Morpher extends React.Component {
 
       t1.play();
     }
+
+    //animate viewbox of the SVG
     let newViewBox = toSVG.getAttribute("viewBox");
-    // fromSVG.setAttribute("viewBox", newViewBox);
     let t2 = timeline();
     t2.animate({
       targets: fromSVG,
@@ -219,6 +224,47 @@ class Morpher extends React.Component {
       }
     });
     t2.play();
+  }
+
+  equalizeNumOfPaths(from, to) {
+    let fromSVG = from.parentSVG;
+    let toSVG = to.parentSVG;
+    let fromPaths = from.paths;
+    let toPaths = to.paths;
+
+    if (fromPaths.length - toPaths.length !== 0) {
+      let case1 = fromPaths.length < toPaths.length;
+      let case2 = fromPaths.length > toPaths.length;
+      let diff = case1
+        ? toPaths.length - fromPaths.length
+        : case2
+        ? fromPaths.length - toPaths.length
+        : 0;
+      for (let i = 0; i < diff; i++) {
+        let elemPath = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "path"
+        );
+        let copyPath = case1
+          ? fromPaths[fromPaths.length - 1]
+          : toPaths[toPaths.length - 1];
+        elemPath.setAttributeNS(null, "d", copyPath.getAttribute("d"));
+        if (case1) {
+          fromSVG.getElementsByTagName("g")[0].appendChild(elemPath);
+          fromPaths.push(elemPath);
+        } else {
+          toSVG.getElementsByTagName("g")[0].appendChild(elemPath);
+          toPaths.push(elemPath);
+        }
+      }
+    }
+  }
+
+  sortString(a, b) {
+    let aPath = a.getAttribute("d");
+    let bPath = b.getAttribute("d");
+
+    return bPath.length - aPath.length;
   }
 
   interpolateViewBox(left, right) {
@@ -234,15 +280,16 @@ class Morpher extends React.Component {
 
   morph(container) {
     this.counter =
-      this.counter === this.state.svgs.length - 1 ? 0 : this.counter + 1;
-    console.log(this.counter);
+      this.counter === this.state.svgData.length - 1 ? 0 : this.counter + 1;
     this.morphSVGElementToArray(container, this.counter);
+    this.forceUpdate();
   }
 
   render() {
-    let stateSVG = this.state.svgs[0];
+    let stateSVG = this.state.svgData[0].svg;
     const svgHTML = typeof stateSVG !== "undefined" ? stateSVG.outerHTML : "";
-    // console.log(svgHTML);
+
+    document.body.onclick = () => this.morph("container");
 
     return (
       <div className="morpher">
@@ -252,10 +299,10 @@ class Morpher extends React.Component {
           xmlns="http://www.w3.org/2000/svg"
           dangerouslySetInnerHTML={{ __html: svgHTML }}
         ></svg>
-        <hr />
-        <button id="morph-it" onClick={() => this.morph("container")}>
-          Morph It!
-        </button>
+        <p>
+          {this.counter} |{this.state.svgData[this.counter].name}:{" "}
+          {this.state.svgData[this.counter].meaning}
+        </p>
       </div>
     );
   }
